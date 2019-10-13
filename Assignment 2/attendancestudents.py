c5b0f5f3ff599bff96f56b68daea3f5019e325d8
Mahalakshumi Visvanathan <40058339+maha2000@users.noreply.github.com>
2019-10-13 18:57:01 +0530

    Delete  attendancestudents.py

--- Assignment 2/attendancestudents.py
+++
@@ -1,9 +0,0 @@
-n=int(input("enter the no.of students"))
-attendance=[]
-print("Enter your string:")
-for i in range(n): 
-    b=input().split('-')
-    if b[1]=='P':
-       attendance.append(b[0])
-print("People who were present are"+str(attendance))
-
