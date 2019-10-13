4c17a115fb960f4bb6b477896175497c65c3661a
Daicy31 <56192006+Daicy31@users.noreply.github.com>
2019-10-13 15:56:54 +0530

    Create occurances.py

---
+++ Assignment 2/180501031/occurances.py
@@ -0,0 +1,15 @@
+def freq(arr):
+    count = 0;
+    arr.sort()
+    for i in range(len(arr)):
+        if(arr[i] == arr[(i+1)%len(arr)]):
+            count += 1;
+        else:
+            print(arr[i]+"->"+str(count+1))
+            count = 0
+
+
+stg = input("Enter the string:")
+arr = [e for e in stg]
+print("Occurances:")
+freq(arr)
