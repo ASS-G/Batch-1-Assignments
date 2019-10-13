b952aa64bbeda3814adbd6966301f778e1821a10
SRUTHI2000 <45535217+SRUTHI2000@users.noreply.github.com>
2019-10-13 14:54:38 +0530

    SRUTHI S- 2nd year CSE C 180501150

---
+++ Assignment 2/180501150/robberThief.py
@@ -0,0 +1,7 @@
+amt=input("Enter the amount robbed by the thief: ")
+if amt<5000:
+        print("Loss")
+elif amt>=5000 and amt<20000:
+        print("Profit is moderate")
+else:
+        print("Good theft")
