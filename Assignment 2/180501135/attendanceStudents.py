48039d71dc62c312c3495add0bbd5549696d1a01
Seshai Hari <rathought@gmail.com>
2019-10-13 22:48:20 +0530

    Update attendanceStudents.py

--- Assignment 2/180501135/attendanceStudents.py
+++ Assignment 2/180501135/attendanceStudents.py
@@ -13,8 +13,4 @@ for i in s:
         
 
         
-        
-        print "Enter 5 items on shopping list"
-for i in xrange(5): # range starts from 0 and ends at 5-1 (so 0, 1, 2, 3, 4 executes your loop contents 5 times)
-    shopping = raw_input()
-    mylist.append(shopping) # add input to the list
+
