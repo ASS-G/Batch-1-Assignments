bdce6d3dcff692ea993272e745b36078d76ad78a
Visalialagappan <56196348+Visalialagappan@users.noreply.github.com>
2019-10-13 18:24:17 +0530

    Create Occurrances.py

---
+++ Assignment 2/180501173/Occurrances.py
@@ -0,0 +1,18 @@
+str1= input("enter the string:")
+
+times={}
+
+print("occurances:\n")
+
+for i in str1:
+   
+ if i in times:
+       
+ times[i]+= 1
+       
+ elif:
+            
+times[i]= 1
+            
+print(str1(times))
+
