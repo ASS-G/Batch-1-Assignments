e9731062356abac89c405f058c2d278ad5336692
Nandhakumar <nandy951@gmail.com>
2019-10-13 11:27:42 +0530

    Nandhakumar - 2nd Year CSE B 180501097

--- Assignment 2/180501097/attendanceStudents.py
+++ Assignment 2/180501097/attendanceStudents.py
@@ -5,4 +5,6 @@ for i in range(n):
     b=input().split('-')
     if b[1] == 'P':
         attendance.append(b[0])
-print('People who were present are : '+str(attendance))
+print('People who were present are : ')
+for name in attendance:
+    print(name, end =" ")
