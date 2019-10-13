e53650ada26fc44e778928a5107e1cca988aa444
unknown <kumarantr2000@gmail.com>
2019-10-13 19:13:32 +0530

    KUMARAN TR: CS B Second year 180501081

---
+++ Assignment 1/180501081/occurances.py
@@ -0,0 +1,6 @@
+s="aabbccddee"
+print("a -> ",s.count('a'))
+print("b -> ",s.count('b'))
+print("c -> ",s.count('c'))
+print("d -> ",s.count('d'))
+print("e -> ",s.count('e'))
---
+++ Assignment 1/180501081/robberThief.py
@@ -0,0 +1,9 @@
+n=int(input("Enter the amount robbed by the thief:"))
+if n<5000:
+     print('Loss')
+elif n>5000 and n<19999:
+      print('Profit is Moderate') 
+else :
+      print('Good')
+ #else:
+     # print('Invalid')
