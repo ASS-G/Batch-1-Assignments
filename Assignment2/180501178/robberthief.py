6e4d3b5aeec90909b0b0c429ab63903e134850c1
Vishnupriyavelmurugan <56183627+Vishnupriyavelmurugan@users.noreply.github.com>
2019-10-13 19:44:07 +0530

    Create robberthief.py

---
+++ Assignment2/180501178/robberthief.py
@@ -0,0 +1,7 @@
+amount=int(input('enter the amount robbed by the thief: '))
+if amount<5000:
+    print("loss")
+elif (amount>=5000) and (amount<=19999):
+    print("profit is moderate")
+else:
+    print("good theft")
