d042a68fad268fed7929e9dcd82aa01daf4774f1
Subramanian Muthupalaniappan <palaniappans2000@outlook.com>
2019-10-13 16:10:07 +0530

    Palaniappan S - 2nd Year CSE B 18050105

---
+++ Assignment 2/180501105/attendanceStudents.py
@@ -0,0 +1,6 @@
+attendance=['Ram-P','Malar-A','Ahuja-P','Vijay-P','Harini-A']
+#print("Students present")
+for a in attendance:
+    if a[-1]=='P':
+        print(a)
+
---
+++ Assignment 2/180501105/occurences.py
@@ -0,0 +1,5 @@
+from collections import Counter as c
+s=input("Enter a string:")
+a=c(s)
+print(a)
+
