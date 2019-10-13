37c18ee6e1ff11f3d4b8405ce9714b816f7b7d2c
Subramanian Muthupalaniappan <palaniappans2000@outlook.com>
2019-10-13 17:21:44 +0530

    Palaniappan S - 2nd Year CSE B 180501105

--- Assignment 2/180501105/attendanceStudents.py
+++ Assignment 2/180501105/attendanceStudents.py
@@ -1,6 +1,15 @@
-attendance=['Ram-P','Malar-A','Ahuja-P','Vijay-P','Harini-A']
+#attendance=['Ram-P','Malar-A','Ahuja-P','Vijay-P','Harini-A']
 #print("Students present")
-for a in attendance:
-    if a[-1]=='P':
-        print(a)
+#for a in attendance:
+#   if a[-1]=='P':
+#       print(a)
+a=int(input('Enter the No of Students : '))
+h=[]
+print('Enter your string : ')
+for i in range(a):
+	b=input().split('-')
+	if b[1] == 'P':
+		h.append(b[0])
+print('Students Present: '+str(h))
+
 
