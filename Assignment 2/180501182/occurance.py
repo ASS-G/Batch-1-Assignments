e0370055cd1668133146bd95c08caa4d5b954df1
harshithaprakash09 <harshithaprakash9@gmail.com>
2019-10-13 18:27:04 +0530

    Harshitha Y:CS C 180501182

---
+++ Assignment 2/180501182/occurance.py
@@ -0,0 +1,15 @@
+string=input("Enter the string: ")
+print("Occurances:")
+count = string.count('a')
+print('a ->', count)
+count = string.count('b')
+print('b ->', count)
+count = string.count('c')
+print('c ->', count)
+count = string.count('d')
+print('d ->', count)
+count = string.count('e')
+print('e ->', count)
+
+
+
