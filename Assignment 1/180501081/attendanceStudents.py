a9cc8b5cf3fb570e9715e3d088c801ee57645c65
KUMARANTR <53537511+KUMARANTR@users.noreply.github.com>
2019-10-13 21:58:52 +0530

    Update attendanceStudents.py

--- Assignment 1/180501081/attendanceStudents.py
+++ Assignment 1/180501081/attendanceStudents.py
@@ -1,8 +1,11 @@
-n=int(input("enter the no.of students"))
-attendance=[]
-print("Enter your string:")
-for i in range(n): 
-    b=input().split('-')
-    if b[1]=='P':
-       attendance.append(b[0])
-print("People who were present are"+str(attendance))
+l=[]
+n=int(input("Enter no. of student:"))
+print("Enter your string(name):")
+for i in range(0,n):
+    a=str(input())
+    l.append(a)
+print("people who were  present are :")
+for m in l:
+    k="P" in m
+    if(k):
+        print(m[0:-2],end="  ")
