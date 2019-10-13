df9c4a536c5c6c0927b7491b131c51d60ea76994
swetha7799 <56219464+swetha7799@users.noreply.github.com>
2019-10-13 22:59:25 +0530

    Swetha S - 2nd Year CSE C 180501157 (#166)
    * Swetha.S: CS C 180501157
    
    * Tej Kiran D M: CSE C 2nd yr 180501161
    
    * Tej kiran D M 2ndyr cse c 180501161
    
    * Delete hello_world.py
    
    * Create robberThief.py
    
    * Create occurances.py
    
    * Create attendanceStudents.py
    
    * Delete hello_world.py

---
+++ attendanceStudents.py
@@ -0,0 +1,11 @@
+n=int(input('enter the no.of students:'))
+attendance=[]
+print('enter the string:')
+for i in range(n)
+     b=input().split('-')
+     if b[1]=='p'
+         attendance.append(b[0])
+print('people  present are:')
+for name in attendance:
+    print(name,end ="")
+
