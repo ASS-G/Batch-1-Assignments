aa6e0a4974074a49edb85ea154b4345e9c052ef8
Seshai Hari <rathought@gmail.com>
2019-10-13 22:44:19 +0530

    Update attendanceStudents.py

--- Assignment 2/180501135/attendanceStudents.py
+++ Assignment 2/180501135/attendanceStudents.py
@@ -1,4 +1,7 @@
-s = ["Ram-P", "Malar-A", "Ahuja-P", "Vijay-P", "Harini-A"]
+print("Enter Names of 5 students:(ex: Harini-P):")
+for j in range(5):
+    name = raw_input()
+    s.append(name)
 print('The present students were:')
 b=0
 print("The present students were:")
@@ -8,3 +11,10 @@ for i in s:
         print(k[:-2]);
     b+=1;
         
+
+        
+        
+        print "Enter 5 items on shopping list"
+for i in xrange(5): # range starts from 0 and ends at 5-1 (so 0, 1, 2, 3, 4 executes your loop contents 5 times)
+    shopping = raw_input()
+    mylist.append(shopping) # add input to the list
