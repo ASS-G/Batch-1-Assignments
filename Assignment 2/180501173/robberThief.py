78ffd94810dad00442a72ce238461e2574dfd79a
Visalialagappan <56196348+Visalialagappan@users.noreply.github.com>
2019-10-13 20:46:16 +0530

    Visali Alagappan - 2nd Year CSE C 180501173 (#154)
    * Create robberThief.py
    
    * Create Occurrances.py
    
    * Create attendanceStudent.py

---
+++ Assignment 2/180501173/robberThief.py
@@ -0,0 +1,15 @@
+val= int
+
+val= int(input("enter the amount robbed by the thief:"))
+
+if val<5000:
+    
+print("loss")
+   
+ elif val>=5000 and val<=19999:
+     
+   print("profit is moderate")
+       
+ else:
+           
+ print("good theft")
