ba5c85f1db106bc276b28d113d7793f28ef9f9fb
Gopika321 <56188113+Gopika321@users.noreply.github.com>
2019-10-14 21:44:30 +0530

    Create attendaceStudent.py

---
+++ 180501092/attendaceStudent.py
@@ -0,0 +1,8 @@
+csv = input("Enter name - attendance status (enter multiple values using comma): ")
+s = csv.split(',')
+for i in range(len(s)):
+    if s[i].find("P"):
+        print(s[i].split('-'))
+    else:
+        pass
+ 
