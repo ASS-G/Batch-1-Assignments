e908d8223451880c5510eb1b84cefa3fcb9d80ac
Gopika321 <56188113+Gopika321@users.noreply.github.com>
2019-10-14 21:46:07 +0530

    Create robberThief.py

---
+++ 180501092/robberThief.py
@@ -0,0 +1,13 @@
+
+amt = input("Enter the amount robbed by the thief:")
+a = int(amt)
+if a < 5000:
+    print('Profit is Loss')
+elif a >= 5000 or a <= 19999:
+    print('Profit is Moderate')
+else:
+    print('Profit is Good Theft')
+ 
+
+ 
+
