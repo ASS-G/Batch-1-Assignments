ff2ad701cc55d10c062c07e804c1a474fea26ce1
Vijay08Natraj07 <56229493+Vijay08Natraj07@users.noreply.github.com>
2019-10-19 21:32:35 +0530

    Vijay Natraj K - 2nd Year CSE C 180501170 (#175)
    * Vijay08Natraj07: CS C 180501170
    
    * Vijay08Natraj07 CS C 180501170

---
+++ Assignment 2/180501170/attedanceStudent.py
@@ -0,0 +1,12 @@
+lst=[]
+n=int(input("Enter the number of students :"))
+for i in range(n):
+	name=input("Enter name")
+	lst.append(name)
+print(lst)
+for i in lst:
+	if  i.endswith('P'):
+		print(i.split("-")[0])
+	if  i.endswith('p'):
+		print(i.split("-")[0])
+		
---
+++ Assignment 2/180501170/robberThief.py
@@ -0,0 +1,8 @@
+amount=int(input("Enter the amount robbed ny thief"))
+if amount<5000:
+	print("Profit is Loss")
+elif amount==20000:
+	print("Profit is Good")
+elif amount>5000: 
+	if amount<19999:
+	 	print("Profit is Moderate")
